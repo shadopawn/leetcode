6e9fd9609988e5078fcbb6958b30ed4667297064
Daniel Litt <daniel_litt@comcast.net>
2022-04-12 11:09:56 -0400

    Add queue question from microsoft

---
+++ create-a-queue/create_a_queue.py
@@ -0,0 +1,28 @@
+example_input = [(1, 42), (2, -1), (1, 14), (3, -1), (1, 28),
+                 (3, -1), (1, 60), (1, 70), (2, -1), (2, -1), (3, -1)]
+
+queue = []
+
+
+def enqueue(value):
+    queue.append(value)
+
+
+def dequeue(_):
+    return queue.pop()
+
+
+def peek(_):
+    print(queue[-1])
+
+
+function_map = {1: enqueue, 2: dequeue, 3: peek}
+
+
+def process_input(input_list):
+    for function_key, value in input_list:
+        current_function = function_map[function_key]
+        current_function(value)
+
+
+process_input(example_input)
